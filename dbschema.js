120bb25a0c006dc0b2a9cee3181682dc5d0bf5b8
tayyabasikander <tayyaba.sikander2000@gmail.com>
2020-04-29 22:47:36 +0500

    initial commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-04-28T21:50:50.989Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ functions/index.js
@@ -0,0 +1,53 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
+
+//Deploy
+// admin.initializeApp();
+
+//Serve
+var serviceAccount = require("../functions/privateKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://socialape2-32116.firebaseio.com"
+});
+
+app.get('/screams', (req, res) => {
+    admin.firestore().collection('screams').orderBy('createdAt','desc').get()
+            .then(data => {
+                let screams = [];
+                data.forEach(doc => {
+                    screams.push({
+                        screamId: doc.id,
+                        body: doc.data().body,
+                        userHandle: doc.data().userHandle,
+                        createdAt: doc.data().createdAt
+                    });
+                });
+                return res.json(screams);
+            })
+            .catch(err => console.error(err));
+})
+
+app.post('/scream', (req, res) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
+    };
+
+    admin.firestore().collection('screams').add(newScream)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created successfully`});
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong'})
+            console.error(err)
+        })
+});
+
+//  https://baseurl.com/api/
+
+exports.api = functions.https.onRequest(app);
