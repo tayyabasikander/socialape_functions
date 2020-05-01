58621f562dde273bb34935d4513ce1bd9226c5c3
tayyabasikander <tayyaba.sikander2000@gmail.com>
2020-05-02 00:50:05 +0500

    worked on upload image

---
+++ functions/util/admin.js
@@ -0,0 +1,16 @@
+const admin = require('firebase-admin');
+
+//Deploy
+// admin.initializeApp();
+
+//Serve
+var serviceAccount = require("../../functions/privateKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://socialape2-32116.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
---
+++ functions/util/firebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDG7WTlHUn9_2h4PEw4PA07DWxNpzpafLE",
+    authDomain: "socialape2-32116.firebaseapp.com",
+    databaseURL: "https://socialape2-32116.firebaseio.com",
+    projectId: "socialape2-32116",
+    storageBucket: "socialape2-32116.appspot.com",
+    messagingSenderId: "621044492779",
+    appId: "1:621044492779:web:59b2c9753bf71dfa5e51ae"
+  };
